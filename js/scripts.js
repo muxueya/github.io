3607c12d6ff9a86a0a9fad4d455ebce8880f6a1e
muxueya <muxueya@live.cn>
2024-08-22 22:47:09 +0200

    add a slide on navigation bar

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,31 @@ for (let i = 0; i < numStars; i++) {
     starfield.appendChild(star);
 }
 
+$(document).ready(function () {
+    $(".nav-link").on("mouseover", function () {
+        var position = $(this).position(); // Use position relative to the navbar
+        var width = $(this).outerWidth(); // Use outerWidth for full width
+        $(".slide2")
+            .css({ opacity: 1, left: position.left, width: width })
+            .addClass("squeeze");
+    });
+
+    $(".nav-link").on("mouseout", function () {
+        $(".slide2").css({ opacity: 0 }).removeClass("squeeze");
+    });
+
+    $(".nav-link").on("mousemove", function () {
+        var position = $(this).position(); // Use position relative to the navbar
+        var width = $(this).outerWidth(); // Use outerWidth for full width
+        $(".slide1").css({ left: position.left, width: width });
+    });
+
+    // Initial positioning
+    var currentWidth = $(".navbar-nav .nav-item:first-child").outerWidth();
+    var current = $(".navbar-nav .nav-item:first-child").position();
+    $(".slide1").css({ left: current.left, width: currentWidth });
+});
+
 // Smooth Scroll for links with # in href
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
